cfd54a39ee34a6eb32c4eb950c0668d9e4c67cb9
gurunathreddy <gurunathreddy2727@gmail.com>
2025-06-20 14:39:49 +0530

    deploy to replit

--- app_diabets.py
+++ app_diabets.py
@@ -31,5 +31,10 @@ def predict():
 
 
 
+# if __name__ == "__main__":
+#     app.run(debug=True)
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 3000))  # Replit uses this PORT env variable
+    app.run(host='0.0.0.0', port=port, debug=True)
